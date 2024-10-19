c282e672cadef06089fbe5451c841c8f0e3fe73d
Felipeex <felipeexx48@gmail.com>
2024-10-18 21:30:34 -0300

    .

--- algorithm-II/files/index.cpp
+++ algorithm-II/files/index.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+struct exampleStruct {
+  int exampleNumber;
+};
+
+
 int main() {
+  exampleStruct example;
   FILE * coolFilePointer = fopen("coolfile.dat", "wb");
+
+  scanf("%d", &example.exampleNumber);
+
+  fwrite(&example, sizeof(exampleStruct), 1, coolFilePointer);
 
   fclose(coolFilePointer);
 }
